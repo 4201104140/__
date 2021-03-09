8d37cff0f3557693f87a09fe139258888d1d4cc0
Phan Tấn Tài <phantantai9800@gmail.com>
2021-03-09 09:34:28 +0700

    tai

---
+++ GitTrends/GitTrends/GitTrends.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Content;
+using Xamarin.Forms;
+
+namespace GitTrends.Droid
+{
+    [Activity(Label = "GitTrends", Icon = "@mipmap/icon", RoundIcon = "@mipmap/icon_round", Theme = "@style/LaunchTheme", LaunchMode = LaunchMode.SingleTop, MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    [IntentFilter(new string[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "gittrends" })]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.SetTheme(Resource.Style.MainTheme);
+            base.OnCreate(savedInstanceState);
+
+            Forms.Init(this, savedInstanceState);
+
+            var app = 
+            LoadApplication();
+        }
+        
+    }
+}
---
+++ PowerToys/src/runner/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <winrt/base.h>
+#include <d3d11_2.h>
+#include <d2d1_3.h>
+
+#include <Windows.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.h>
+
+#include <wil/resource.h>
