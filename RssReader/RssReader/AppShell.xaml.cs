a72fc9e3119f668b2d18c19bf422610320719ef8
Phan Tấn Tài <phantantai9800@gmail.com>
2021-09-22 20:27:37 +0700

    tai

---
+++ RssReader/RssReader/AppShell.xaml.cs
@@ -0,0 +1,35 @@
+﻿using RssReader.Controls;
+using RssReader.ViewModels;
+using RssReader.Views;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace RssReader
+{
+    /// <summary>
+    /// The "chrome" layer of the app that provides top-level navigation with
+    /// proper keyboarding navigation.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public static AppShell Current = null;
+
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public AppShell()
+        {
+            this.InitializeComponent();
+            Current = this;
+
+
+        }
+
+        public Frame AppFrame => AppSh
+    }
+}
---
+++ RssReader/RssReader/NavMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace RssReader
+{
+    /// <summary>
+    /// Data to represent an item in the nav menu.
+    /// </summary>
+    public class NavMenuItem
+    {
+        public string Label { get; set; }
+        public Symbol Symbol { get; set; }
+        public char SymbolAsChar => (char)Symbol;
+        public Type DestPage { get; set; }
+        public object Arguments { get; set; }
+    }
+}
